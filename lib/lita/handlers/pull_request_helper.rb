83f37d88be8681e66ebaf8b78528a6dcdf948e1b
Arturo Puente <arturopuentevc@gmail.com>
2016-08-27 13:06:14 -0500

    Improve method names

--- lib/lita/handlers/pull_request_helper.rb
+++ lib/lita/handlers/pull_request_helper.rb
@@ -8,26 +8,25 @@ class PullRequestHelper
   end
 
   def retrieve
-    issues.select do |issue|
-      resolve_assignee(issue, username)
+    pull_requests.select do |pull|
+      assigned_to_user?(pull, username)
     end
   end
 
-  def issues
-    results = []
+  def pull_requests
     client.org_issues("platanus", filter: "all", per_page: 100)
     last_response = client.last_response
-    results.concat(last_response.data)
+    issues = last_response.data
     until last_response.rels[:next].nil?
       last_response = last_response.rels[:next].get
-      results.concat(last_response.data)
+      issues.concat(last_response.data)
     end
-    results.select { |issue| issue[:pull_request].nil? }
+    issues.reject { |issue| issue[:pull_request].nil? }
   end
 
-  def resolve_assignee(issue, username)
-    return true if issue[:assignee] && issue[:assignee][:login] == username
-    return true if issue[:assignees] && issue[:assignees].any? { |a| a[:login] == username }
+  def assigned_to_user?(pull, username)
+    return true if pull[:assignee] && pull[:assignee][:login] == username
+    return true if pull[:assignees] && pull[:assignees].any? { |a| a[:login] == username }
     false
   end
 
